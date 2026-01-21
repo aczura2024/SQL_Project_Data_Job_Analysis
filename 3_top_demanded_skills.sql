df2be0eebdef32de70b6817dbafa5ed182a7b7f4
George Thomas <george.everywhere@outlook.com>
2026-01-21 17:43:12 -0500

    Update SQL query for top skills of data analysts in Canada
    Modified the SQL query to focus on job postings in Canada and limited the results to the top 5 in-demand skills for data analysts.

--- 3_top_demanded_skills.sql
+++ 3_top_demanded_skills.sql
@@ -6,6 +6,9 @@ Question: What are the most in-demand skills for data analysts?
 - Why? Retrieves the top 5 skills with the highest demand in the job market,
 providing insights into the most valuable skills for job seekers
 */
+
+
+-- Jobs in Canada
 SELECT
     skills,
     COUNT(skills_job_dim.job_id) AS demand_count
@@ -14,7 +17,7 @@ INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst' AND 
-    job_work_from_home = TRUE
+    job_location LIKE '%Canada%'
 GROUP BY
     skills
 ORDER BY
