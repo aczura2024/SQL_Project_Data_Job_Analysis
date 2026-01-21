1c84182522fd8c2266aa9d88a9d5f04f966efdf3
George Thomas <george.everywhere@outlook.com>
2026-01-21 17:41:44 -0500

    Refactor SQL query for top-paying Data Analyst jobs

--- 1_top_paying_jobs.sql
+++ 1_top_paying_jobs.sql
@@ -8,30 +8,26 @@ Question: What are the top-paying data analyst jobs?
 
 -- Jobs in Canada
 
-WITH top_paying_jobs AS (
-    SELECT
-        job_id,
-        job_title,
-        salary_year_avg,
-        name AS company_name
-    FROM
-        job_postings_fact
-    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+-- Jobs in Canada
 
-    WHERE
-        job_title_short = 'Data Analyst' AND 
-        job_location LIKE '%Canada%' AND
-        salary_year_avg IS NOT NULL
-    ORDER BY
-        salary_year_avg DESC    
-    Limit 10
-)
+SELECT
+    job_id,
+    job_title,
+    job_title_short,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 
-SELECT 
-    top_paying_jobs.*,
-    skills
-FROM top_paying_jobs
-INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_location LIKE '%Canada%' 
+    AND salary_year_avg IS NOT NULL
 ORDER BY
-    salary_year_avg DESC
+    salary_year_avg DESC   
+Limit 10
+
